747b3d08544064501dcf7226f9d6b4d53ca5994c
MidnightTale <middnighttale24@gmail.com>
2023-09-29 15:09:48 +0700

    Reload Initialize database

--- src/main/java/xyz/hynse/hyeconomy/Command/ReloadCommand.java
+++ src/main/java/xyz/hynse/hyeconomy/Command/ReloadCommand.java
@@ -3,12 +3,17 @@ package xyz.hynse.hyeconomy.Command;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import xyz.hynse.hyeconomy.Hyeconomy;
+import xyz.hynse.hyeconomy.Util.HikariCPUtil;
 import xyz.hynse.hyeconomy.Util.MessageUtil;
 public class ReloadCommand {
     public static void execute(Player player) {
        if (player.hasPermission("hyecomoney.reload")) {
+           if (HikariCPUtil.dataSource != null) {
+               HikariCPUtil.dataSource.close();
+           }
             Hyeconomy.instance.reloadConfig();
             MessageUtil.loadMessagesConfig();
+            HikariCPUtil.initializeDataSource(Hyeconomy.instance.getConfig());
             player.sendMessage((Component) MessageUtil.getMessage("general.reloadSuccess"));
         } else {
             player.sendMessage((Component) MessageUtil.getMessage("general.noPermission"));
